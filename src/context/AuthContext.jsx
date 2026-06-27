8d7b19c6775fbf83afb45f6c5745106d2a228260
durandavid159-arte <durandavid159@gmail.com>
2026-06-26 23:34:47 -0500

    DP creo la logica para que el usuario pueda autenticarse en todas las pantallas

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useState, useEffect, useContext } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../services/firebaseConfig.js';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            setLoading(false);
+        });
+
+        
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ currentUser, loading }}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+
+    /*temporal*/
+    onAuthStateChanged(auth, (user) => {
+
+    console.log("Usuario autenticado:");
+    console.log(user);
+
+    setCurrentUser(user);
+    setLoading(false);
+    });
+
+    /*Temporal*/
+    onAuthStateChanged(auth, (user) => {
+
+        console.log("Usuario autenticado:");
+        console.log(user);
+
+        setCurrentUser(user);
+        setLoading(false);
+    });
+
+};
+
+export const useAuth = () => useContext(AuthContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles/globals.css';
-import './styles/components.css';
-
-import { AuthProvider } from './context/AuthContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-    <App />
+      <App />
     </AuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
